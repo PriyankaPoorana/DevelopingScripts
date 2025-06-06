896ee3f8b25ea6d5bc044404247e11e4103f380d
PriyankaPoorana <46278604+PriyankaPoorana@users.noreply.github.com>
2025-06-06 14:55:04 +0100

    Update bank_balance_calculation.py

--- bank_balance_calculation.py
+++ bank_balance_calculation.py
@@ -25,36 +25,3 @@ def main():
 
 if __name__=="__main__":
     main()
-
-newstring=[]
-count=1
-for i in range(1,len(string)):
-  if (string[i]==string[i-1]):
-    count+=1
-  elif string[i]==string[-1]:
-    newstring.append(string[i]+str(count))
-  else:
-    newstring.append(string[i-1]+str(count))
-    count=1
-print(''.join(newstring))
-
-for i in range(0,len(l1)):
-  if l1[i]%3==0 and l1[i]%5==0:
-    l2.append("FizzBizz")
-  elif l1[i]%3==0:
-    l2.append("Fizz")
-  elif l1[i]%5==0:
-    l2.append("Bizz")
-  else:
-    l2.append(l1[i])
-
-    str1='a gentleman'
-    str2='elegant man'
-
-    l1=str1.split('')
-    l2=str2.split('')
-
-for i in range(0,n):
-   l1=[0,1]
-   if n>2:
-    
